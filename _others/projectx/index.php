2b241d77a7ee6b889ba53770b289f7221b1ce29c
master <hosting@404.ge>
2019-01-30 15:39:01 -0600

    login theme add

--- _others/projectx/index.php
+++ _others/projectx/index.php
@@ -1,37 +1,99 @@
-<?php
-session_start();
-header('X-Frame-Options: DENY');
-header("Content-type: text/html; charset=utf-8");
-$dir_explode = explode("index.php",__FILE__);
-define("DIR",$dir_explode[0]);
-// Deny requesting GLOBALS
-if (ini_get('register_globals'))
-{
-    (isset($_REQUEST['GLOBALS']) OR isset($_FILES['GLOBALS'])) AND exit(1);
-    $global_variables = array_keys($GLOBALS);
-    $global_variables = array_diff($global_variables, array(
-                '_COOKIE', '_ENV', '_GET',
-                '_FILES', '_POST', '_REQUEST',
-                '_SERVER', '_SESSION', 'GLOBALS'
-            ));
-    foreach ($global_variables as $name)
-        unset($GLOBALS[$name]);
-}
-date_default_timezone_set("Asia/Tbilisi");
-@include("config.php");
-function __autoload($class_name) 
-{
-    $filename = str_replace('_', DIRECTORY_SEPARATOR, strtolower($class_name)).'.php';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Login V10</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->  
+    <link rel="icon" type="image/png" href="template/img/icons/favicon.ico"/>
+<!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="template/vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="template/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="template/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+<!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="template/vendor/animate/animate.css">
+<!--===============================================================================================-->  
+    <link rel="stylesheet" type="text/css" href="template/vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="template/vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="template/vendor/select2/select2.min.css">
+<!--===============================================================================================-->  
+    <link rel="stylesheet" type="text/css" href="template/vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="template/css/util.css">
+    <link rel="stylesheet" type="text/css" href="template/css/main.css">
+<!--===============================================================================================-->
+</head>
+<body>
+    
+    <div class="limiter">
+        <div class="container-login100">
+            <div class="wrap-login100 p-t-50 p-b-90">
+                <form class="login100-form validate-form flex-sb flex-w">
+                    <span class="login100-form-title p-b-51">
+                        Login
+                    </span>
 
-	$file = $filename;
-	if (!file_exists($file))
-    {
-        echo "Class: <b>".$class_name."</b> can't load.."; exit();
-    }
-    @include $file;
-}
+                    
+                    <div class="wrap-input100 validate-input m-b-16" data-validate = "Username is required">
+                        <input class="input100" type="text" name="username" placeholder="Username">
+                        <span class="focus-input100"></span>
+                    </div>
+                    
+                    
+                    <div class="wrap-input100 validate-input m-b-16" data-validate = "Password is required">
+                        <input class="input100" type="password" name="pass" placeholder="Password">
+                        <span class="focus-input100"></span>
+                    </div>
+                    
+                    <div class="flex-sb-m w-full p-t-3 p-b-24">
+                        <div class="contact100-form-checkbox">
+                            <input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
+                            <label class="label-checkbox100" for="ckb1">
+                                Remember me
+                            </label>
+                        </div>
 
-$bootstrap = new lib_lancher_bootstrap();
-$bootstrap->lanch($c); 
+                        <div>
+                            <a href="#" class="txt1">
+                                Forgot?
+                            </a>
+                        </div>
+                    </div>
 
-?>
+                    <div class="container-login100-form-btn m-t-17">
+                        <button class="login100-form-btn">
+                            Login
+                        </button>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    </div>
+    
+
+    <div id="dropDownSelect1"></div>
+    
+<!--===============================================================================================-->
+    <script src="template/vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+    <script src="template/vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+    <script src="template/vendor/bootstrap/js/popper.js"></script>
+    <script src="template/vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+    <script src="template/vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+    <script src="template/vendor/daterangepicker/moment.min.js"></script>
+    <script src="template/vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+    <script src="template/vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+    <script src="template/js/main.js"></script>
+
+</body>
+</html>
